cf2e485d7bcac92f731ed656150363245bca21bc
MaxOhn <ohn.m@hotmail.de>
2024-11-27 13:44:51 +0100

    doc: add backticks

--- src/model/verification.rs
+++ src/model/verification.rs
@@ -7,11 +7,11 @@ pub enum Verification {
     /// Verification key assigned by the o!rdr dev. DM MasterIO#4588 on Discord to get one.
     Key(Box<str>),
     /// Simulates a request that will successfully render a video. Listen to the websocket to get the state of this fake render
-    /// (render_added, render_progress and render_done are emitted).
+    /// (`render_added`, `render_progress` and `render_done` are emitted).
     DevModeSuccess,
     /// Simulates a request that will fail on the API level. No websocket events are emitted.
     DevModeFail,
-    /// Simulates a request that will fail on the Websocket level (render_failed event is emitted).
+    /// Simulates a request that will fail on the Websocket level (`render_failed` event is emitted).
     DevModeWsFail,
 }
 
